05b8f6a56ef7d62d6771bd3099c8b3e8be921a80
Ivan Zinovyev <vanyazin@gmail.com>
2016-03-11 03:50:56 +0300

    All test are passing

--- src/lugate.lua
+++ src/lugate.lua
@@ -9,21 +9,32 @@ local json = require "rapidjson"
 local Lugate = {}
 
 -- Class constructor
-function Lugate.new(raw_body)
-    local self = setmetatable({}, Lugate)
+function Lugate:new(raw_body)
+    local lugate = setmetatable({}, Lugate)
     self.__index = self
     self.raw_body = raw_body
 
-    return self
+    return lugate
 end
 
 -- Get table data
 function Lugate:get_data()
---    if self.data == nil then
---        self.data = json.decode(self.raw_body)
---    end
---
---    return self.data
+    if self.data == nil then
+        self.data = json.decode(self.raw_body)
+    end
+
+    return self.data
+end
+
+-- Check if request is a batch
+function Lugate:is_batch()
+    -- todo
+end
+
+-- Check if single request is valid
+function Lugate:is_valid()    -- todo
+
+    -- todo
 end
 
 return Lugate
