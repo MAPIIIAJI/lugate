f6cddf83e1ffa0be1f17b71a6e082b511d303f7a
Ivan Zinovyev <vanyazin@gmail.com>
2016-03-13 01:44:16 +0300

    Structured and styled

--- README.md
+++ README.md
@@ -1,4 +1,30 @@
-# lugate
-Lua module for building JSON-RPC 2.0 Gateway APIs just inside of your Nginx
+# Lugate
+Lugate is a lua module for building JSON-RPC 2.0 Gateway APIs just inside of your Nginx configuration file.
 
 [![Build Status](https://travis-ci.org/zinovyev/lugate.svg?branch=master)](https://travis-ci.org/zinovyev/lugate)
+
+# Example
+```lua
+-- Get request body
+body = ngx.req.get_body_data()
+-- Get new lugate instance
+lugate = Lugate:new({
+  body = body,
+  routes = {
+    "example.com/v1", "^v1%..+",
+    "service01.example.com/v2", "^v2%.math%s.+",
+    "service02.example.com/v2", "^v2%.string%s.+",
+  }
+})
+-- Get requests collection
+if lugate:is_valid() then
+  -- Send several parrallel subrequests
+  results = {ngx.location.capture_multi(lugate:get_all)}
+  -- Attach response values on lugate instance
+  lugate:respond(results)
+  ngx.say(lugate.get_response)
+-- Or print the error response on failure
+else
+  ngx.say(lugate.err_parse_error)    
+end
+```
--- src/lugate.lua
+++ src/lugate.lua
@@ -1,44 +1,69 @@
---
--- lugate.lua
---
+----------------------
+-- The lugate module.
+-- Lugate is a lua module for building JSON-RPC 2.0 Gateway APIs just inside of your Nginx configuration file.
+-- Lugate is meant to be used with [ngx\_http\_lua\_module](https://github.com/openresty/lua-nginx-module) together.
+-- @module lugate
+-- @author Ivan Zinovyev <vanyazin@gmail.com>
+-- @license MIT
 
 -- Json encoder/decoder
 local json = require "rapidjson"
 
--- Gateway class
-local Lugate = {}
+--- The lua gateway class
+local Lugate = {
+  body = nil, -- Request raw body
+  routes = {}, -- Routing rules
+}
 
--- Class constructor
-function Lugate:new(raw_body)
-    local lugate = setmetatable({}, Lugate)
-    self.__index = self
-    self.raw_body = raw_body
+--- Create new Lugate instance
+-- @param[type=string] body Request raw body
+-- @param[type=table] routes Routing rules
+-- @return[type=Lugate] The new instance of Lugate
+function Lugate:new(body, routes)
+  local lugate = setmetatable({}, Lugate)
+  self.__index = self
+  self.body = body
+  self.routes = routes or {}
 
-    return lugate
+  return lugate
 end
 
--- Get table data
-function Lugate:get_data()
-    if self.data == nil then
-        self.data = json.decode(self.raw_body)
+--- Get the collection of requests
+-- @return[type=table] The
+function Lugate:get_request()
+  if not self.request then
+    self.request = {}
+    self.data = json.decode(self.body)
+    if self.data then
+        print(self.data)
     end
+  end
 
-    return self.data
+  return self.request
 end
 
--- Check if request is a batch
-function Lugate:is_batch()
-    return (self.data[1] ~= nil)
-            and self:is_valid(self.data[1])
-end
-
--- Check if single request is valid
-function Lugate:is_valid(data)
-    return (type(data) == 'table')
-            and (data.jsonrpc ~= nil)
-            and (data.method ~= nil)
-            and (data.params ~= nil)
-            and (data.id ~= nil)
-end
+---- Get table data
+--function Lugate:get_data()
+--  if self.data == nil then
+--    self.data = json.decode(self.raw_body)
+--  end
+--
+--  return self.data
+--end
+--
+---- Check if request is a batch
+--function Lugate:is_batch()
+--  return (self.data[1] ~= nil)
+--    and self:is_valid(self.data[1])
+--end
+--
+---- Check if single request is valid
+--function Lugate:is_valid(data)
+--  return (type(data) == 'table')
+--    and (data.jsonrpc ~= nil)
+--    and (data.method ~= nil)
+--    and (data.params ~= nil)
+--    and (data.id ~= nil)
+--end
 
 return Lugate
