527e483211d7d8be60d4e918f660cc5b48a260df
Ivan Zinovyev <vanyazin@gmail.com>
2016-06-27 18:00:12 +0300

    Trying to guess if the response is properly formatted json

--- src/lugate.lua
+++ src/lugate.lua
@@ -236,11 +236,14 @@ function Lugate:run()
     for n, response in ipairs(responses) do
       self.responses[self.req_dat.num[n]] = self:clean_response(response)
 
-      -- Process empty or broken responses
+      -- Quick way to find broken responses
+      local first_char = string.sub(self.responses[self.req_dat.num[n]], 1, 1);
+      local last_char = string.sub(self.responses[self.req_dat.num[n]], -1);
       local broken = false
-      if '' == self.responses[self.req_dat.num[n]] then
+      if ('' == self.responses[self.req_dat.num[n]]) or ('{' ~= first_char and '[' ~= first_char) or ('}' ~= last_char and ']' ~= last_char) then
+          -- Process empty or broken responses
           self.responses[self.req_dat.num[n]] = self:clean_response(self:build_json_error(
-              Lugate.ERR_SERVER_ERROR, 'Server error. Empty response.', nil, self.req_dat.ids[n]
+              Lugate.ERR_SERVER_ERROR, 'Server error. Bad response.', nil, self.req_dat.ids[n]
           ))
           broken = true
       end
