0d19e4da77d583b6d4634e9b1e5b737b749f6cac
Ivan Zinovyev <vanyazin@gmail.com>
2016-10-05 16:54:17 +0300

    Better HTTP response parsing

--- spec/lugate_spec.lua
+++ spec/lugate_spec.lua
@@ -145,3 +145,45 @@ describe("Check request factory", function()
     assert.equal(3, #lugate:get_requests())
   end)
 end)
+
+describe("Check response validation", function ()
+  local ngx = { req = {}, HTTP_OK = 200 }
+  local lugate = Lugate:new({ ngx = ngx, json = require "rapidjson" })
+  local bad_response = {
+    status = 504,
+    body = [[
+<!DOCTYPE html>
+<html>
+<head>
+<title>Error</title>
+<style>
+    body {
+        width: 35em;
+        margin: 0 auto;
+        font-family: Tahoma, Verdana, Arial, sans-serif;
+    }
+</style>
+</head>
+<body>
+<h1>An error occurred.</h1>
+<p>Sorry, the page you are looking for is currently unavailable.<br/>
+Please try again later.</p>
+<p>If you are the system administrator of this resource then you should check
+the <a href="http://nginx.org/r/error_log">error log</a> for details.</p>
+<p><em>Faithfully yours, nginx.</em></p>
+</body>
+</html>
+      ]],
+  }
+  it("Should provide a valid HTTP error status", function()
+    lugate.req_dat.num[1] = 1
+    lugate.req_dat.ids[1] = 256
+    lugate:handle_response(1, bad_response)
+    print(lugate.responses[1])
+--    ngx.req.get_body_data = function()
+--      return '{"foo":"bar"}'
+--    end
+--    assert.equal(1, #lugate:get_requests())
+  end)
+
+end)
